0a1b6ae456a3a60d67e61d4189937b44595428de
Donbazile07 <donbazile007@gmail.com>
2022-05-03 17:05:57 -0400

    build firestore project

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import { addRestaurant, getAllRestaurant} from './src/restaurants.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+app.post('/restaurants', addRestaurant);
+app.get('/restaurants', getAllRestaurant);
 
 app.listen(3000, () => {
     console.log('listening on http://localhost:3000...');
